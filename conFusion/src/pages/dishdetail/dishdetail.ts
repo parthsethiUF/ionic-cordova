2935161f00964d0c7a9f23e8308d2e63c360d823
parth <parthsethi16.ps@gmail.com>
2018-07-29 21:30:26 -0400

    alerts toast loading

--- conFusion/src/pages/dishdetail/dishdetail.ts
+++ conFusion/src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 
@@ -25,9 +25,11 @@ export class DishdetailPage {
 
   favorite: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
     @Inject('BaseURL') private BaseURL,
-    private favoriteservice: FavoriteProvider) {
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController) {
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length;
@@ -43,5 +45,11 @@ export class DishdetailPage {
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+
+    this.toastCtrl.create({
+      message: 'Dish ' + this.dish.id + ' added as favorite successfully',
+      position: 'middle',
+      duration: 3000
+    }).present();
   }
 }
--- conFusion/src/pages/reservation/reservation.ts
+++ conFusion/src/pages/reservation/reservation.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, , ViewController } from 'ionic-angular';
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Generated class for the ReservationPage page.
  *
@@ -14,9 +14,15 @@ import { IonicPage, NavController, NavParams, , ViewController } from 'ionic-ang
   templateUrl: 'reservation.html',
 })
 export class ReservationPage {
-
+  reservation: FormGroup;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private formBuilder: FormBuilder) {
+    this.reservation = this.formBuilder.group({
+      guests: 3,
+      smoking: false,
+      dateTime: ['', Validators.required],
+    });
   }
 
   ionViewDidLoad() {
@@ -27,5 +33,9 @@ export class ReservationPage {
     this.viewCtrl.dismiss();
   }
 
+  onSubmit() {
+    console.log(this.reservation.value);
+    this.viewCtrl.dismiss();
+  }
 
 }
